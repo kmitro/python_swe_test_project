04d05f54bf2281310dd713d3913c41d7b8715a36
Kirill Mitrofanenko <kirill.mitrofanenko@t-systems.com>
2024-08-21 21:25:20 +0200

    reworked for swe usage

---
+++ kmitro_python_swe_test_project/__init__.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kmitro_python_swe_test_project',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your project dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            # Define command-line scripts here
+        ],
+    },
+)
